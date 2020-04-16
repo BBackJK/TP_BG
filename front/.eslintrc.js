4b25b8406639d59f978ef1057c7ddf2ba0e755b2
BBackJK <wnsrud6839@gmail.com>
2020-04-16 11:03:15 +0900

    [FRONT] Add eslint rules

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
-	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:vue/essential',
-		'prettier',
-		'plugin:prettier/recommended'
-	],
-	plugins: ['prettier'],
-	rules: {
-		'prettier/prettier': [
-			'error',
-			{
-				singleQuote: true,
-				semi: true,
-				useTabs: false,
-				tabWidth: 2,
-				printWidth: 80,
-				bracketSpacing: true,
-				arrowParens: 'avoid'
-			}
-		],
-		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-	}
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto'
+      }
+    ],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
 };
