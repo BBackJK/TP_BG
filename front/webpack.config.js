1df38502223c5ce096ea355e4c28d0c14bfb13b1
bbackjk <bbackjk1113@gmail.com>
2020-04-29 14:37:31 +0900

    [FRONT] Add build script and webpack bundle option

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -57,4 +57,8 @@ module.exports = {
     new VueLoaderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
   ],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+  }
 };
